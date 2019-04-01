cf6c06ff1073bfdad1a63f261602bc910ecb2411
Auguste Valley <a.valley@baypath.net>
2019-04-01 13:11:00 -0400

    need to fix download link

--- COE/config/initializers/assets.rb
+++ COE/config/initializers/assets.rb
@@ -7,6 +7,10 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.precompile += %w( agency.js )
+Rails.application.config.assets.precompile += %w( jquery.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap/js/bootstrap.bundle.min.js )
+Rails.application.config.assets.precompile += %w( jquery.easing.min.js )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
